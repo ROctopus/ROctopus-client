71ff0d545f5f5eb98c9d8cbd4df18ff68aea441f
vankesteren <erikjan.vankesteren@gmail.com>
2017-10-24 23:56:45 +0200

    add first makewinstaller nsi script + debug py

--- scripts/winbuild.bat
+++ scripts/winbuild.bat
@@ -1,6 +1,6 @@
 %PYINSTALLER% ^
     --noconfirm --log-level=INFO ^
-    --onedir --windowed ^
+    --onedir --debug ^
     --name="rocto-client" ^
     --icon=rocto_client\Qt\ui\source\icons\rocto_icon.ico ^
     rocto_client\__main__.py
