ff5140daa2401a1f9ed1539c1810a59ff52fc5e2
vankesteren <erikjan.vankesteren@gmail.com>
2017-10-24 22:35:53 +0200

    Update winbuild for download of zip

--- scripts/winbuild.bat
+++ scripts/winbuild.bat
@@ -1,5 +1,5 @@
 pyinstaller --noconfirm --log-level=INFO ^
-    --onefile --windowed ^
-    --name="rocto" ^
+    --onedir --windowed ^
+    --name="rocto-client" ^
     --icon=rocto_client\Qt\ui\source\icons\rocto_icon.ico ^
     rocto_client\__main__.py
